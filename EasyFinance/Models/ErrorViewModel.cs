78c7b7a2cc24bbb9df9c8d93537fe8b7c5424511
Matheus <Matheus@DESKTOP-3LBIOVD>
2024-01-03 21:58:21 -0300

    Adicionar arquivos de projeto.

---
+++ EasyFinance/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EasyFinance.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Platform/Entity/User.cs
@@ -0,0 +1,13 @@
+﻿namespace Platform.Entity
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
